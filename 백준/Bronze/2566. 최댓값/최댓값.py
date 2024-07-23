70fcd4abecf5fba35341d1f738c14240f0f10722
eunji0306 <168632897+eunji0306@users.noreply.github.com>
2024-07-23 15:38:38 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,17 @@
+#2566
+a=[]
+for _ in range(9):
+    a.append(list(map(int,input().split())))
+max=0
+row=0
+col=0
+
+for i in range(9):
+  for j in range(9):
+    if a[i][j]>=max:
+      max=a[i][j]
+      row=i+1
+      col=j+1
+
+print(max)
+print(row, col)
