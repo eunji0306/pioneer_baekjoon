e5a827259926f5f1dc87225acd11eda2142f708d
eunji0306 <168632897+eunji0306@users.noreply.github.com>
2024-08-11 21:41:14 +0900

    [Silver V] Title: 소트인사이드, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -3,13 +3,13 @@ a=list(input())
 temp=0
 
 for i in range(0, len(a)):
-  min=i
+  max=i
   for j in range(i+1, len(a), 1):
-    if a[min]<a[j]:
-      min=j
+    if a[max]<a[j]:
+      max=j
   temp=a[i]
-  a[i]=a[min]
-  a[min]=temp
+  a[i]=a[max]
+  a[max]=temp
 
 for i in a:
   print(i, end='')
